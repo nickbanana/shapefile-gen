655b0a557be6430d6a83bad57a59e60d378b367c
Nick Chi <nick.chi@gmail.com>
2022-06-25 23:53:44 +0800

    實作輸入geojson字串轉成shapefile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
+declare var shpwrite: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'shapefile-gen';
+  formGroup!: FormGroup;
+
+  get geoJsonCtrl() {
+    return this.formGroup.get('geoJson');
+  }
+  constructor(
+    protected fb: FormBuilder
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.formGroup = this.fb.group({
+      geoJson: [null]
+    });
+  }
+
+  download() {
+    const option: {[key: string]: string | object} = {
+      file: 'shapefiles',
+      folder: 'shapefiles',
+      types: {
+        point: 'points',
+      }
+    };
+    if (this.geoJsonCtrl !== null) {
+      shpwrite.download(JSON.parse(this.geoJsonCtrl.value), option);
+    }
+  }
 }
